f29014fa3ab3f46acd4fd191973f26152dca48d0
Andrey <kadyska21@gmail.com>
2022-10-04 20:42:50 +0300

    создали цикл

--- myproject/Program.cs
+++ myproject/Program.cs
@@ -1,3 +1,13 @@
 ﻿// Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам.
 
 string[] Array = new string[8] { "ма", "geek", "brain", "08", ":)", "GeekBrain", "up", "down" };
+int count = 0;
+for (int i = 0; i < Array.Length; i++)
+{
+    Console.Write($"{Array[i]},");
+    if (Array[i].Length < 4)
+    {
+        count++;
+    }
+}
+Console.WriteLine();
