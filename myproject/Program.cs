14ea370be06ba6d74017e4fdd7ae2cf91c98c8c6
Andrey <kadyska21@gmail.com>
2022-10-04 20:45:26 +0300

    создали новый массив

--- myproject/Program.cs
+++ myproject/Program.cs
@@ -11,3 +11,14 @@ for (int i = 0; i < Array.Length; i++)
     }
 }
 Console.WriteLine();
+string[] Array2 = new string[count];
+int j = 0;
+for (int i = 0; i < Array.Length; i++)
+{
+    if (Array[i].Length < 4)
+    {
+        Array2[j] = Array[i];
+        Console.Write($"{Array2[j]},");
+        j++;
+    }
+}
